2028b05c763d90cefab26a132c2edd292ca7347b
BaRaN31313131313131 <98013321+BaRaN31313131313131@users.noreply.github.com>
2022-01-19 10:43:17 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+const aoijs = require("aoi.js");
+
+const bot = yeni aoijs.Bot({
+
+belirteç: "OTMyNzMwNjU4NTU0NTExMzkw.YeXPHg.x031ADfPTVZhpgTXXcwUTHmNCs0"
+
+önek: ["s!"], //Discord Bot Öneki
+
+niyetler: "tümü",
+
+fetchInvites :{ etkin : true },
+
+mobilePlatform: false, // Anlaşmazlık Amaçları
+
+})
+
+const ses = yeni aoijs.Voice(bot)
+
+ })
+
+ //
+
+//Olaylar
+
+bot.onLeave()
+
+bot.onJoin()
+
+bot.onMessage({
+
+  loncaOnly:yanlış
+
+})
+
+const yükleyici = yeni aoijs.LoadCommands(bot)
+
+    loader.load(bot.cmd,"./commands/")
+
+//
+
+//
+
+//[KODLAR YUKARI]
+
+bot.değişkenler({
+
+  var1:"değer1",
+
+  var2:"değer2"
+
+})
+
+bot.durum({
+
+  text: "s!yardım ile komutlarımı görebilirsiniz",
+
+  tür: "OYNAMAK",
+
+  durum: "çevrimiçi",
+
+  zaman: 12
+
+}) 
+
